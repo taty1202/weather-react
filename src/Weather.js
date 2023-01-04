a38eba78479cc1d66cf30e103da478825cb769f5
taty1202 <115900680+taty1202@users.noreply.github.com>
2023-01-04 14:23:44 -0800

    removed files

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
-import "./Weather.css";
+import "./weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
